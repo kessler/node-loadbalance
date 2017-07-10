1e5007c34de31a63127e4e84786d9cada031e972
kessler <yanivk@gmail.com>
2017-07-10 22:04:32 +0300

    export WeightedRandomEngine, change random(...)
    - random(...) will return a RandomEngine or WeightedRandomEngine
    based on the contents of the pool's first member

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var AbstractEngine = module.exports.AbstractEngine = require('./lib/AbstractEngine.js')
-module.exports.random = module.exports.RandomEngine = require('./lib/RandomEngine.js')
+var RandomEngine = module.exports.RandomEngine = require('./lib/RandomEngine.js')
+var WeightedRandomEngine = module.exports.WeightedRandomEngine = require('./lib/WeightedRandomEngine.js')
 var RoundRobinEngine = module.exports.RoundRobinEngine = require('./lib/RoundRobinEngine.js')
 var WeightedRoundRobinEngine = module.exports.WeightedRoundRobinEngine = require('./lib/WeightedRoundRobinEngine.js')
  
@@ -14,6 +15,20 @@ module.exports.roundRobin = function(pool) {
 		return new WeightedRoundRobinEngine(pool)
 	} else {
 		return new RoundRobinEngine(pool)
+	}
+}
+
+module.exports.random = function(pool, seed) {
+	if (pool.length === 0) {
+		throw new Error('pool length must be greater than zero')
+	}
+
+	var entry = pool[0]
+
+	if (entry.weight) {
+		return new WeightedRandomEngine(pool, seed)
+	} else {
+		return new RandomEngine(pool, seed)
 	}
 }
 
